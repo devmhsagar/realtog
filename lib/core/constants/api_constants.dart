7158392cc04303482c4249747b1608bd73d32561
Linkon Siddique <linkon.sq48@gmail.com>
2025-12-05 00:14:14 +0600

    Added authentication urls

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,14 @@
+class ApiConstants {
+  static const String baseUrl = "https://basic-nodejs-backend.onrender.com";
+
+  // Auth endpoints
+  static const String loginUrl = "/auth/login";
+  static const String registerUrl = "/auth/register";
+  static const String currentUserUrl = "/auth/me";
+  static const String googleAuthUrl = "/auth/google";
+  static const String googleAuthCallbackUrl = "/auth/google";
+  static const String googleAuthSuccessUrl = "/auth/google/success";
+  static const String googleAuthFailureUrl = "/auth/google/failure";
+  static const String forgotPasswordUrl = "/auth/forgot-password";
+  static const String resetPasswordUrl = "/auth/reset-password";
+}
