ad90b57b9fa154e1daa22fabaeaa90ffd206b216
Linkon Siddique <linkon.sq48@gmail.com>
2025-12-05 00:24:30 +0600

    Added dio service

---
+++ lib/core/services/http_services.dart
@@ -0,0 +1,56 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import '../constants/api_constants.dart';
+
+class DioService {
+  // Singleton
+  static final DioService _instance = DioService._internal();
+  factory DioService() => _instance;
+
+  DioService._internal() {
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: ApiConstants.baseUrl,
+        connectTimeout: const Duration(seconds: 100),
+        receiveTimeout: const Duration(seconds: 100),
+      ),
+    );
+
+    _dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) async {
+          // get token from secure storage
+          final token = await _storage.read(key: "access_token");
+          if (token != null) {
+            options.headers["Authorization"] = "Bearer $token";
+          }
+          return handler.next(options);
+        },
+        onError: (DioException e, handler) async {
+          if (e.response?.statusCode == 401) {
+            await _storage.deleteAll();
+            // TODO: Add navigation to login page if needed
+          }
+          return handler.next(e);
+        },
+      ),
+    );
+  }
+
+  late Dio _dio;
+  final _storage = const FlutterSecureStorage();
+
+  Dio get client => _dio;
+}
+
+// Usage Example
+// final dio = DioService().client;
+
+// Future<void> fetchUserProfile() async {
+//   try {
+//     final response = await dio.get("/user/profile");
+//     print(response.data);
+//   } catch (e) {
+//     print("Error: $e");
+//   }
+// }
