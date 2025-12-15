04663cd15c863c339b9d23f9241a7996610de9a1
Linkon Siddique <linkon.sq48@gmail.com>
2025-12-15 23:46:55 +0600

    Removed firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,11 @@
-import 'dart:io' show Platform;
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:realtog/core/router/app_router.dart';
 import 'package:realtog/core/theme/theme.dart';
 
-import 'firebase_options.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  // Initialize Google Sign-In once at app startup
-  // On iOS, explicitly pass the clientId from Firebase options
-  // On Android, it will be read from google-services.json
-  await GoogleSignIn.instance.initialize(
-    clientId: Platform.isIOS ? DefaultFirebaseOptions.ios.iosClientId : null,
-  );
 
   runApp(const ProviderScope(child: App()));
 }
