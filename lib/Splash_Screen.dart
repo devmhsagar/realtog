e91452e8eed4ed5ab7b742bbbbb07de705980aa0
devmhsagar <mmehedihasansagar@gmail.com>
2026-02-05 19:57:42 +0600

    Splash Screen

---
+++ lib/Splash_Screen.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:realtog/pages/home/home_page.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            FlutterLogo(size: 120),
+            SizedBox(height: 20),
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
